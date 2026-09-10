fe1c36b91ca8ba82931276648ddac43803516a3b
Piyush raj <piyushamarpur2@gmail.com>
2026-09-10 12:10:56 +0530

    Add files via upload

---
+++ usecase3.java
@@ -0,0 +1,44 @@
+package oops;
+
+//UseCase3
+interface Rentable{
+    static double TAX_RATE =0.05;
+    double calculateRent(int days);
+}
+
+class Car implements Rentable{
+    static int totalVehiclesRented =0;
+    Car(){
+        totalVehiclesRented++;
+    }
+    public double calculateRent(int days){
+        return 1500*days;
+    }
+
+}
+class Bike implements Rentable{
+    Bike(){
+        Car.totalVehiclesRented++;
+    }
+    public double calculateRent(int days){
+        return 500*days;
+    }
+
+}
+
+
+
+public class usecase2 {
+    public static void main(String[] args) {
+        Rentable car = new Car();
+        Rentable bike = new Bike();
+
+        double carRent = car.calculateRent(3);
+        double bikeRent = bike.calculateRent(2);
+
+        System.out.println("Car rent incl. tax: " + (carRent + carRent * Car.TAX_RATE));
+        System.out.println("Bike rent incl. tax: " + (bikeRent + bikeRent * Bike.TAX_RATE));
+        System.out.println("Total vehicles rented: " + Car.totalVehiclesRented);
+    }
+
+}
